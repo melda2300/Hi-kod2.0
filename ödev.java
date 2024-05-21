83aadab2c28b3ab5256f786d0489e41cb3de0025
melda <146105556+melda2300@users.noreply.github.com>
2024-05-21 19:26:08 +0300

    ödev.java

---
+++ ödev.java
@@ -0,0 +1,36 @@
+class MainClass {
+  int number = 10;
+  String text = "Hello World!";
+  double decimal = 3.14;
+  bool trueFalse = true;
+
+  var camelCaseNumber = number;
+  var snakeCaseText = text;
+  var pascalCaseDecimal = decimal;
+
+  String name = "John";
+  String surname = "Doe";
+  int age = 30;
+  bool isGraduated = true;
+
+  void printAll() {
+    print('number: $number');
+    print('text: $text');
+    print('decimal: $decimal');
+    print('trueFalse: $trueFalse');
+
+    print('camelCaseNumber: $camelCaseNumber');
+    print('snakeCaseText: $snakeCaseText');
+    print('pascalCaseDecimal: $pascalCaseDecimal');
+
+    print('Name: $name');
+    print('Surname: $surname');
+    print('Age: $age');
+    print('Is graduated?: $isGraduated');
+  }
+}
+
+void main() {
+  MainClass mainClass = MainClass();
+  mainClass.printAll();
+}
